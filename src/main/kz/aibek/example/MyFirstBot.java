76995433e15234566902fb1723752bc6f07f6776
aibek <aibek9898@gmail.com>
2019-04-05 11:24:42 +0600

    added first simple bot

---
+++ src/main/kz/aibek/example/MyFirstBot.java
@@ -0,0 +1,32 @@
+package kz.aibek.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class MyFirstBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "MyFirstBot";
+    }
+
+    public String getBotToken() {
+        return "855714235:AAFa0p_mwy_IEpp2ob_SUwAkq1sqaRWvrRs";
+    }
+}
